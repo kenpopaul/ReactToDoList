57605f9e84542bd670644c032ad51705088162eb
Paul Nix <kenpopaul@yahoo.co.uk>
2023-05-16 07:06:13 +0100

    add id to tasks

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,16 @@ function App() {
   };
 
   const addTask = () => {
-    setTodoList([...todoList, newTask]);
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      taskName: newTask,
+    };
+
+    setTodoList([...todoList, task]);
   };
 
-  const deleteTask = (taskName) => {
-    setTodoList(todoList.filter((task) => task !== taskName));
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
   };
 
   return (
@@ -27,8 +32,8 @@ function App() {
         {todoList.map((task) => {
           return (
             <div>
-              <h1>{task}</h1>
-              <button onClick={() => deleteTask(task)}>X</button>
+              <h1>{task.taskName}</h1>
+              <button onClick={() => deleteTask(task.id)}>X</button>
             </div>
           );
         })}
