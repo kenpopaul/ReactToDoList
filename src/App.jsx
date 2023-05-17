8eb91fb6a5a162935aca04cca2abd8072291018e
Paul Nix <kenpopaul@yahoo.co.uk>
2023-05-17 20:58:55 +0100

    Improved CSS

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
       <div className="list">
         {todoList.map((task) => {
           return (
-            <div>
+            <div className="task">
               <h1>{task.taskName}</h1>
               <button onClick={() => deleteTask(task.id)}>X</button>
             </div>
